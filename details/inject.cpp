12ea92ccac2a43b629fc04385f7c51a4b3372990
Yi Jianlong <jianlong.yi@Outlook.com>
2020-03-02 10:19:43 +0800

    initial commit of c++ template book code

---
+++ details/inject.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+class C {
+ private:
+  int i[2];
+
+ public:
+  static int f() { return sizeof(C); }
+};
+int C;
+int f() { return sizeof(C); }
+
+int main() {
+  std::cout << "C::f() = " << C::f() << ',' << " ::f() = " << ::f() << '\n';
+}
